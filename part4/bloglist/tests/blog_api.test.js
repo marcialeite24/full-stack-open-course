4138c262ab24285c50205d916cb719a6ca4dcf90
marcialeite24 <38685980+marcialeite24@users.noreply.github.com>
2026-04-26 23:06:45 +0100

    Add API tests for GET /api/blogs and refactor to async/await (exercise 4.8)

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,43 @@
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '8.8.4.4'])
+
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  { title: 'First blog', author: 'Author One', url: 'http://example.com/1', likes: 3 },
+  { title: 'Second blog', author: 'Author Two', url: 'http://example.com/2', likes: 7 },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct number of blogs is returned', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
+
