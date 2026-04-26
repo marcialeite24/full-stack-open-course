caa74f2a42e57c59e53751f760691a7db1e40b08
marcialeite24 <38685980+marcialeite24@users.noreply.github.com>
2026-04-26 17:54:12 +0100

    Add mostBlogs and mostLikes helper functions with tests (exercises 4.6-4.7)

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,8 +10,30 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce((favorite, blog) => blog.likes > favorite.likes ? blog : favorite)
 }
 
+const mostBlogs = (blogs) => {
+  const counts = blogs.reduce((result, blog) => {
+    result[blog.author] = (result[blog.author] || 0) + 1
+    return result
+  }, {})
+
+  const author = Object.keys(counts).reduce((a, b) => counts[a] > counts[b] ? a : b)
+  return { author, blogs: counts[author] }
+}
+
+const mostLikes = (blogs) => {
+  const likes = blogs.reduce((result, blog) => {
+    result[blog.author] = (result[blog.author] || 0) + blog.likes
+    return result
+  }, {})
+
+  const author = Object.keys(likes).reduce((a, b) => likes[a] > likes[b] ? a : b)
+  return { author, likes: likes[author] }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
