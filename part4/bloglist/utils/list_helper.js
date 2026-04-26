3f1010029c8b97b99c45c9e54f2ae87970b039da
marcialeite24 <38685980+marcialeite24@users.noreply.github.com>
2026-04-26 17:23:39 +0100

    Add favoriteBlog helper function with tests (exercise 4.5)

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,8 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((favorite, blog) => blog.likes > favorite.likes ? blog : favorite)
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
